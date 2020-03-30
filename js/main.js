c0a6656973e4028ad7b53e7dd6e238e0cefb696e
Misha <abachyanm@gmail.com>
2020-03-30 14:52:30 +0400

    Fix

--- js/main.js
+++ js/main.js
@@ -128,10 +128,39 @@ $('#latest__news__prev').click(function () {
 
 //Rating
 
-// $('.hit__item_rate > ul > li >a').on('click', function () {
-//   $(this).prevAll('.hit__item_rate ul li .fas')
-// });
+$(document).ready(function(){
+    $('.stars li').on('mouseover', function(){
+        var onStar = parseInt($(this).data('value'), 10);
 
-// $('.hit__tem_like').on('click', function () {
-//     $(this).
-// }).
+        $(this).parent().children('li.star').each(function(e){
+            if (e < onStar) {
+                $(this).addClass('hover');
+            }
+            else {
+                $(this).removeClass('hover');
+            }
+        });
+
+    }).on('mouseout', function(){
+        $(this).parent().children('li.star').each(function(e){
+            $(this).removeClass('hover');
+        });
+    });
+
+    $('.stars li').on('click', function(){
+        var onStar = parseInt($(this).data('value'), 10); // The star currently selected
+        var stars = $(this).parent().children('li.star');
+
+        for (i = 0; i < stars.length; i++) {
+            $(stars[i]).removeClass('selected');
+        }
+
+        for (i = 0; i < onStar; i++) {
+            $(stars[i]).addClass('selected');
+        }
+    });
+});
+
+$('.hit__tem_like').on('click', function () {
+    $(this).find('.fas').addClass('liked')
+});
